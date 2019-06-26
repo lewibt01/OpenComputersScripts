f530b926060f6ece6275e5bea6a92fdcfb4d7e07
lewibt01 <lewibt01@pfw.edu>
2019-06-25 22:52:51 -0400

    Create programs.lua

---
+++ programs.lua
@@ -0,0 +1,22 @@
+{
+  --default installation path
+  path="/usr",
+  --Additional repositories and packages go here, for correct package syntax, check https://github.com/OpenPrograms/Vexatos-Programs/blob/master/oppm/etc/example-config.cfg
+  repos={
+    ["lewibt01/OpenComputersScripts"] = {
+      ["nanomachine-control"] = {
+        files = {
+          ["master/TabletPrograms/NanomachineControl.lua"] = "/"
+        },
+        dependencies={},
+        name = "NanomachineControl",
+        descriptions = "Allows manual control of nanomachines within wifi range.",
+        authors = "birdini",
+        note = "No additional notes here.",
+        hidden = true,
+        repo = "tree/master/TabletPrograms"
+      }
+    }
+  }	
+}
+
