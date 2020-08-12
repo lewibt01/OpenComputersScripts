eea5b00305b9e325cac899e8c9171054c2230e5f
lewibt01 <21316163+lewibt01@users.noreply.github.com>
2020-08-12 02:08:23 -0400

    Update github.lua
    github api now correctly parses all chunks of a retrieved file instead of just the first one.
    
    Commented out unused method to avoid problems.

--- APIs/github.lua
+++ APIs/github.lua
@@ -13,8 +13,14 @@ function github.readFile(name)
 end
 ]]
 function github.requestFile(name)
-    local data = internet.request(github.baseURL..name)()
-    return data
+    local handle = internet.request(github.baseURL..name)
+    local result = ""
+
+    for chunk in handle do
+        result = result..chunk
+    end
+
+    return result
 end
 
 function github.pull(name,dest)
@@ -25,8 +31,9 @@ function github.pull(name,dest)
     file:close()
 end
 
+--[[
 function github.clone(name,folderPath)
-    local data = github.ReadFile(name)
+    local data = github.readFile(name)
     
     --ensure the folder path has a / at the end for when we append the file name
     if(folderPath[#folderPath-1] ~= "/") then
@@ -37,6 +44,6 @@ function github.clone(name,folderPath)
     local file = io.open(folderPath..name,"w")
     file:write(data)
     file:close()
-end
+end]]
 
 return github
