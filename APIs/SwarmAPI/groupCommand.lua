0b793200c524998bf3289063d28da075d5afd6fb
lewibt01 <21316163+lewibt01@users.noreply.github.com>
2020-08-16 21:41:05 -0400

    renamed group control api

---
+++ APIs/SwarmAPI/groupCommand.lua
@@ -0,0 +1,20 @@
+local component = require("component")
+local modem = component.modem
+local t = require("commandTranslate")
+local command = require("command")
+
+local gCommand = {}
+gCommand.hostAddress = "ec54a72a-1121-4246-82c4-d39212481987" 
+gCommand.clients = {}
+gCommand.comPort = 2000
+gCommand.responsePort = 2001
+
+
+--blindly commands the clients to perform the given command
+function gCommand.groupCommand(cmd)
+	for c in gCommand.clients do
+		command.send(cmd)
+	end
+end
+
+return gCommand
