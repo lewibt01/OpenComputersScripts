25d7936dd949b5fd04002c5243af4e62f239b249
lewibt01 <21316163+lewibt01@users.noreply.github.com>
2020-08-19 16:23:24 -0400

    Update register.lua

--- ComputerPrograms/register.lua
+++ ComputerPrograms/register.lua
@@ -2,13 +2,13 @@ local r = require("registration")
 local arg = { ... }
 
 function main(input)
-	if(input == "host")
-		r.listen()
-	elseif(input == "join")
-		r.announce()
-	else
-		print("Usage: registration <host/join>")
-	end
+    if(input == "host")
+        r.listen()
+    elseif(input == "join")
+        r.announce()
+    else
+        print("Usage: registration <host/join>")
+    end
 end
 
 main(table.unpack(arg))
